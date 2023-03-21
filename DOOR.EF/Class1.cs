0684f8de496628c00072a1b3c465a2a3193d2eb4
bertgibbons <bertgibbons@CVP2921-BRBS1X2>
2023-03-21 16:33:10 -0400

    Add project files.

---
+++ DOOR.EF/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace DOOR.EF
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Server/Models/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DOOR.Server.Models;
+
+public class ApplicationUser : IdentityUser
+{
+}
